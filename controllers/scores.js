f762711e178348d8b77b48ffbee59bd1d86253ba
spfox24 <68411909+spfox24@users.noreply.github.com>
2020-11-18 21:55:32 -0600

    adds score create to index

--- controllers/scores.js
+++ controllers/scores.js
@@ -2,10 +2,33 @@ const Score = require('../models/score');
 
 module.exports = {
     index,
+    show,
+    new: newScore,
+    create
 }
 
 function index(req, res) {
     Score.find({}, function(err, scores) {
         res.render('scores/index', { scores });
+    });
+}
+
+function show(req, res) {
+    Score.findById(req.params.id, function(err, scores) {
+        res.render('scores/show', { scores });
+    });
+}
+
+function newScore(req, res) {
+    res.render('scores/new', { title: 'Add Score' });
+}
+
+function create(req, res) {
+    
+    const score = new Score(req.body);
+    
+    score.save(function(err) {
+        if(err) return res.render('scores/new');
+        res.redirect('/scores');
     });
 }
--- routes/scores.js
+++ routes/scores.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 
 router.get('/', scoresCtrl.index);
-
+router.get('/new', scoresCtrl.new);
+router.get('/:id', scoresCtrl.show);
+router.post('/', scoresCtrl.create);
 
 
 module.exports = router;
